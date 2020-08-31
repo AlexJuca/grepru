0f2c74a7d555906e0bb562583ba72703026b8bd7
alexjuca <alexandre.juca@nextbss.co.ao>
2020-08-31 22:39:53 +0100

    implement draft of count feature

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,14 @@ pub mod matcher {
 
     pub fn find_matches(args: &Cli, reader: &mut BufReader<File>) {
         let mut line = String::new();
+        let mut count: u32 = 0;
         for _ in reader.read_to_string(&mut line) {
-            if line.contains(&args.pattern) {
-                println!("|> {}", line);
-            } else {
-                println!("The pattern `{}` was not found in file {}", &args.pattern, std::env::args().nth(2).unwrap());
+            if line.contains(&args.pattern) && args.count == true {
+                println!("{}", count);
+                count += 1;
+            }
+            if line.contains(&args.pattern) && args.count == false {
+                println!("{}", line);
             }
         }
     }
@@ -26,5 +29,7 @@ pub mod cli {
         pub(crate) pattern: String,
         #[structopt(parse(from_os_str), required_unless = "version")]
         pub path: PathBuf,
+        #[structopt(parse(from_flag), short="-c", long="--count", help = "print only a count of selected lines per FILE")]
+        pub(crate) count: bool
     }
 }

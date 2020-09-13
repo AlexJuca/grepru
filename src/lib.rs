d42c20b2454b3f3aa77a058030bdeba4fa4633ce
alexjuca <alexandre.juca@nextbss.co.ao>
2020-09-13 10:51:29 +0100

    implement feature that allows user to print the number of time the selected pattern appears in text

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,36 @@ pub mod matcher {
     use std::io::{BufReader, BufRead, Read};
     use std::fs::File;
     use crate::cli::Cli;
+    use std::ops::BitOr;
+    use std::error::Error;
 
     pub fn find_matches(args: &Cli, reader: &mut BufReader<File>) {
-        let mut line = String::new();
+        let mut text = String::new();
         let mut count: u32 = 0;
-        for _ in reader.read_to_string(&mut line) {
-            if line.contains(&args.pattern) && args.count == true {
-                println!("{}", count);
+        for _ in reader.read_to_string(&mut text) {
+
+        }
+        let pattern = &args.pattern;
+
+        if args.count == true {
+            if pattern.len() > 1 {
+                count = count_words(&text, pattern.parse().unwrap());
+            }
+            println!("{}", count);
+        }
+        if text.contains(&args.pattern) && args.count == false {
+            println!("{}", text);
+        }
+    }
+
+    fn count_words(text: &String, pattern: String) -> u32 {
+        let mut count = 0;
+        text.split_ascii_whitespace().for_each(|n| {
+            if n.contains(&pattern) {
                 count += 1;
             }
-            if line.contains(&args.pattern) && args.count == false {
-                println!("{}", line);
-            }
-        }
+        });
+        return count;
     }
 }
 
